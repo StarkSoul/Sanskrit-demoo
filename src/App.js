bb457457895cfabdea22da0a69adac13959bf9de
Achin Kulshreshtha <61320638+StarkSoul@users.noreply.github.com>
2021-02-06 18:05:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <div>
     <Segment>
       <Header textAlign='center' as='h1'>
-      <Image src='https://sm.mashable.com/t/mashable_in/feature/t/the-oceans/the-oceans-absorbed-an-unfathomable-amount-of-heat-this-deca_e3zb.960.png'>
+      
         <p> प&#2381;रथम&#2307; प&#2366;ठ&#2307;</p> 
-        </Image>
+        
       </Header>
       </Segment>
     </div>
