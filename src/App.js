64b40c20bcda49c4aacc603820f975da649c67e4
Achin Kulshreshtha <61320638+StarkSoul@users.noreply.github.com>
2021-02-06 17:24:13 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> प&#2381;रथम&#2307; प&#2366;ठ&#2307;</p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
