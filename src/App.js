3dfdc1146bc0f9d0207573ebe4cdfe67f2cf3336
Achin Kulshreshtha <61320638+StarkSoul@users.noreply.github.com>
2021-02-06 18:01:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 //import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
+import { Header, Image ,Segment } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+    <Segment>
+      <Header textAlign='center' as='h1'>
+      <Image src='https://sm.mashable.com/t/mashable_in/feature/t/the-oceans/the-oceans-absorbed-an-unfathomable-amount-of-heat-this-deca_e3zb.960.png'>
         <p> प&#2381;रथम&#2307; प&#2366;ठ&#2307;</p> 
-      </header>
+        </Image>
+      </Header>
+      </Segment>
     </div>
   );
-}
+} 
 
 export default App;
