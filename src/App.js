ac06c575880ab85e416d51a9b41123aae714c237
Achin Kulshreshtha <61320638+StarkSoul@users.noreply.github.com>
2021-02-07 16:24:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import logo from './logo.svg';
 //import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
-import { Header, Image ,Segment } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div>
-    <Segment>
-      <Header textAlign='center' as='h1'>
-      
+    <div style={{}}>
+    <Segment style={{borderRadius:90,backgroundColor:'#5993ff',}}>
+      <Header  textAlign='center' as='h1' style={{backgroundColor:"#01034d",
+      color:"White",}}>
         <p> प&#2381;रथम&#2307; प&#2366;ठ&#2307;</p> 
-        
       </Header>
-      </Segment>
+    </Segment>
     </div>
   );
 } 
